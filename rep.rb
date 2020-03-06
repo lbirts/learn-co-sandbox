73c2d45d34ab469897936fd4e87ceab923d2ee20
Lauren Birts <birtslauren@gmail.com>
2020-03-06 00:45:04 +0000

    Automatically backed up by Learn

--- rep.rb
+++ rep.rb
@@ -4,10 +4,10 @@
 #   count += 1 # A bit of work that moves the bit of data closer to being false
 # end
 
-magic_exit_number = 7
-count = 0
-while count < 10 do
-  break if count == magic_exit_number
-  puts "I am the #{count}, I love to count!" # Work
-  count = count + 1
-end
+# magic_exit_number = 7
+# count = 0
+# while count < 10 do
+#   break if count == magic_exit_number
+#   puts "I am the #{count}, I love to count!" # Work
+#   count = count + 1
+# end
